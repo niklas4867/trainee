0917d106db58f69b38c682424f03e62f9305a61d
Alpav <alexander.pavlov@bluewin.ch>
2018-09-21 08:26:34 +0200

    Panels optimiert

--- Bolis/main.Designer.cs
+++ Bolis/main.Designer.cs
@@ -103,6 +103,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 40);
             this.button1.TabIndex = 105;
+            this.button1.Text = "n";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.btnueberweisen_Click);
             // 
